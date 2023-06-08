5f6076a6b5d713a711ca663f3d0fbbcca87a67ee
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-06-08 18:18:20 -0500

    updates to correct date location on page

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  $(".saveBtn").on( "click", function() {
+    console.log( $( this ).siblings("textarea").val());
+    var key=$( this ).parent().attr( "id" )
+    var text=$( this ).siblings("textarea").val()
+    localStorage.setItem(key, text);
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,4 +26,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var today = dayjs().format('MM/DD/YYYY')
+   $("#currentDay").text(today)
 });
+
+// dayjs('2019-01-25').format('DD/MM/YYYY')
+
